117f624e6864490b5322e0caad703880a3314915
Ian Marquez Aleph Labs <ian.regil@aleph-labs.com>
2024-06-05 09:40:26 +0800

    Update: Added word rotation animation in external gsap file

--- gsap.js
+++ gsap.js
@@ -1,1 +1,21 @@
-console.log("test")
+
+// Begin Word Rotation Animation
+(function() {
+  const stagger = 1;
+  const tl = gsap.timeline({
+    repeat: -1,
+  });
+
+  tl.from('.word-rotation-list li', {
+    y: 120,
+    opacity: 0,
+    stagger: stagger
+  })
+
+  tl.to('.word-rotation-list li', {
+    y: -120,
+    opacity: 0,
+    stagger: stagger
+  }, stagger)
+})()
+// End Word Rotation Animation
