e46d8f2b50ae82cbd579edc196c4b73a0ba7e5ec
Ian Marquez Aleph Labs <ian.regil@aleph-labs.com>
2024-07-01 16:57:39 +0800

    Update: added faq mobile and mobile landscape behavior

--- main/components/faq/index.js
+++ main/components/faq/index.js
@@ -1,120 +1,224 @@
 let faqOpenedIndex;
+let isFaqMobile = false;
 
 function closeAccordion(header, content) {
-  if (faqOpenedIndex === undefined || !header || !content) return
-  const openedHeader = header
-  const openedContent = content
+  if (faqOpenedIndex === undefined || !header || !content) return;
+  const openedHeader = header;
+  const openedContent = content;
 
-  const timeline = gsap.timeline()
-  timeline.timeScale(3)
+  const timeline = gsap.timeline();
+  timeline.timeScale(3);
 
   timeline.to(openedContent, {
-    height: 0
-  })
-  timeline.to(openedHeader, {
-    color: "var(--greyscale--fg--subtlest)",
-    textDecoration: "none"
-  }, "<")
-  const [_, openedFAQSymbol] = gsap.utils.toArray(openedHeader.children)
-  timeline.to(openedFAQSymbol, { rotation: 0 }, "<")
+    height: 0,
+  });
+  timeline.to(
+    openedHeader,
+    {
+      color: "var(--greyscale--fg--subtlest)",
+      textDecoration: "none",
+    },
+    "<"
+  );
+  const [_, openedFAQSymbol] = gsap.utils.toArray(openedHeader.children);
+  timeline.to(openedFAQSymbol, { rotation: 0 }, "<");
 
-  faqOpenedIndex = undefined
+  faqOpenedIndex = undefined;
 }
 
-const faq = () => {
-  const faqHeaders = gsap.utils.toArray(".faq-header-div")
-  const faqContent = gsap.utils.toArray(".faq-item-content")
-  const faqFilterChips = gsap.utils.toArray(".faq-filter-chip")
+const FAQBodyAnimation = () => {
+  const faqHeaders = gsap.utils.toArray(".faq-header-div");
+  const faqContent = gsap.utils.toArray(".faq-item-content");
+  const faqFilterChips = gsap.utils.toArray(".faq-filter-chip");
 
-  $("#all-filter").detach().prependTo(".faq-filter-collection-list")
+  $("#all-filter").detach().prependTo(".faq-filter-collection-list");
 
   function closeAccordion() {
-    if (faqOpenedIndex === undefined) return
-    const openedHeader = faqHeaders[faqOpenedIndex]
-    const openedContent = faqContent[faqOpenedIndex]
+    if (faqOpenedIndex === undefined) return;
+    const openedHeader = faqHeaders[faqOpenedIndex];
+    const openedContent = faqContent[faqOpenedIndex];
 
-    const timeline = gsap.timeline()
-    timeline.timeScale(3)
-
+    const timeline = gsap.timeline();
+    timeline.timeScale(3);
 
     timeline.to(openedContent, {
-      height: 0
-    })
-    timeline.to(openedHeader, {
-      color: "var(--greyscale--fg--subtlest)",
-      // textDecoration: "none"
-    }, "<")
-    const [_, openedFAQSymbol] = gsap.utils.toArray(openedHeader.children)
-    timeline.to(openedFAQSymbol, { rotation: 0 }, "<")
+      height: 0,
+    });
+    timeline.to(
+      openedHeader,
+      {
+        color: "var(--greyscale--fg--subtlest)",
+        // textDecoration: "none"
+      },
+      "<"
+    );
+    const [_, openedFAQSymbol] = gsap.utils.toArray(openedHeader.children);
+    timeline.to(openedFAQSymbol, { rotation: 0 }, "<");
 
-    openedIdx = undefined
+    openedIdx = undefined;
   }
-  $("#all-filter").detach().prependTo(".faq-filter-collection-list")
+  $("#all-filter").detach().prependTo(".faq-filter-collection-list");
 
   function openAccordion(header, idx) {
-    const timeline = gsap.timeline()
-    const [_, symbol] = gsap.utils.toArray(header.children)
-    timeline.timeScale(3)
+    const timeline = gsap.timeline();
+    const [_, symbol] = gsap.utils.toArray(header.children);
+    timeline.timeScale(3);
     if (faqOpenedIndex !== undefined) {
       timeline.to(faqContent[faqOpenedIndex], {
-        height: 0
-      })
-      timeline.to(faqHeaders[faqOpenedIndex], {
-        color: "var(--greyscale--fg--subtlest)",
-        textDecoration: "none"
-      }, "<")
+        height: 0,
+      });
+      timeline.to(
+        faqHeaders[faqOpenedIndex],
+        {
+          color: "var(--greyscale--fg--subtlest)",
+          textDecoration: "none",
+        },
+        "<"
+      );
 
-      const [_, openedFAQSymbol] = gsap.utils.toArray(faqHeaders[faqOpenedIndex].children)
-      timeline.to(openedFAQSymbol, { rotation: 0 }, "<")
-
+      const [_, openedFAQSymbol] = gsap.utils.toArray(
+        faqHeaders[faqOpenedIndex].children
+      );
+      timeline.to(openedFAQSymbol, { rotation: 0 }, "<");
     }
-    timeline.to(faqContent[idx], {
-      height: "auto"
-    }, "<")
+    timeline.to(
+      faqContent[idx],
+      {
+        height: "auto",
+      },
+      "<"
+    );
 
-    timeline.to(header, {
-      color: "var(--brand--fg--neutral)",
-      // textDecoration: "underline"
-    }, "<")
+    timeline.to(
+      header,
+      {
+        color: "var(--brand--fg--neutral)",
+        // textDecoration: "underline"
+      },
+      "<"
+    );
 
-    timeline.to(symbol, { rotation: 45 }, "<")
-
+    timeline.to(symbol, { rotation: 45 }, "<");
   }
 
-  faqFilterChips.map(chip => {
-    chip.onclick = () => closeAccordion(faqHeaders[faqOpenedIndex], faqContent[faqOpenedIndex])
-  })
+  faqFilterChips.map((chip) => {
+    chip.onclick = () =>
+      closeAccordion(faqHeaders[faqOpenedIndex], faqContent[faqOpenedIndex]);
+  });
 
   faqHeaders.map((header, idx) => {
     header.onclick = () => {
       if (faqOpenedIndex !== idx) {
-        openAccordion(header, idx)
-        faqOpenedIndex = idx
+        openAccordion(header, idx);
+        faqOpenedIndex = idx;
       } else {
-        closeAccordion(faqHeaders[faqOpenedIndex], faqContent[faqOpenedIndex])
+        closeAccordion(faqHeaders[faqOpenedIndex], faqContent[faqOpenedIndex]);
       }
-    }
-  })
+    };
+  });
+};
+
+function AnimateDropdown() {
+  let dropdownOpen = false;
+  if (!isFaqMobile) return;
+  const faqDropdown = document.getElementById("faq-dropdown-div");
+  const faqFormBlock = document.querySelector(".faq-form-block");
+  const faqFilters = gsap.utils.toArray(".faq-filter-chip-container");
+  const [filterText, symbol] = gsap.utils.toArray(faqDropdown.children);
+  const dropdownTimeline = gsap.timeline();
+  dropdownTimeline.timeScale(3);
+
+  faqFilters.map((filter) => {
+    console.log(filter);
+    filter.addEventListener("click", () => {
+      closeDropdown();
+      filterText.innerText = filter.innerText;
+    });
+  });
+
+  faqDropdown.onclick = () => {
+    if (dropdownOpen) return closeDropdown();
+    return openDropdown();
+  };
+  function closeDropdown() {
+    dropdownOpen = !dropdownOpen;
+    dropdownTimeline
+      .to(symbol, {
+        rotation: 0,
+      })
+      .to(
+        faqFormBlock,
+        {
+          height: 0,
+        },
+        "<"
+      );
+  }
+
+  function openDropdown() {
+    dropdownOpen = !dropdownOpen;
+    dropdownTimeline
+      .to(symbol, {
+        rotation: 180,
+      })
+      .to(
+        faqFormBlock,
+        {
+          height: "auto",
+        },
+        "<"
+      );
+  }
 }
 
+function AttachFAQEventsOnDOMLifecycle() {
+  const mobilePageIndicator = document.getElementById("faq-page-indicator");
+  isFaqMobile = document.body.clientWidth <= 767;
 
-$(document).ready(() => {
-  faq()
+  FAQBodyAnimation();
+  AnimateDropdown();
 
   setTimeout(() => {
-    const pageButtons = gsap.utils.toArray(".faq-page-button")
-    const faqFilters = gsap.utils.toArray(".faq-filter-chip-container")
-    const items = [...pageButtons, ...faqFilters]
+    const pageButtons = gsap.utils.toArray(".faq-page-button");
+    const faqFilters = gsap.utils.toArray(".faq-filter-chip-container");
+    const paginationNavBtns = gsap.utils.toArray(".faq-pagination-nav-button");
 
-    items.map(pageButton => {
-      pageButton.onclick = () => {
-        if (faqOpenedIndex !== undefined) {
-          const faqHeaders = gsap.utils.toArray(".faq-header-div")
-          const faqContent = gsap.utils.toArray(".faq-item-content")
-          closeAccordion(faqHeaders[faqOpenedIndex], faqContent[faqOpenedIndex])
-        }
-        setTimeout(faq, 500)
+    function onButtonClick() {
+      if (faqOpenedIndex !== undefined) {
+        const faqHeaders = gsap.utils.toArray(".faq-header-div");
+        const faqContent = gsap.utils.toArray(".faq-item-content");
+        closeAccordion(faqHeaders[faqOpenedIndex], faqContent[faqOpenedIndex]);
       }
-    })
-  }, 500)
-})
+      setTimeout(FAQBodyAnimation, 500);
+    }
+
+    pageButtons.map((pageButton) => {
+      pageButton.onclick = onButtonClick;
+    });
+
+    faqFilters.map((filter) => {
+      filter.addEventListener("click", () => {
+        onButtonClick();
+        mobilePageIndicator.innerText = 1;
+      });
+    });
+
+    paginationNavBtns.map((paginationBtn) => {
+      paginationBtn.onclick = () => {
+        const buttonType = paginationBtn.classList[0];
+        if (buttonType === "w-pagination-previous") {
+          mobilePageIndicator.innerText =
+            parseInt(mobilePageIndicator.innerText) - 1;
+        } else {
+          mobilePageIndicator.innerText =
+            parseInt(mobilePageIndicator.innerText) + 1;
+        }
+        onButtonClick();
+      };
+    });
+  }, 500);
+}
+
+$(window).on("resize", AttachFAQEventsOnDOMLifecycle);
+
+$(document).ready(AttachFAQEventsOnDOMLifecycle);
--- main/components/navbar/index.js
+++ main/components/navbar/index.js
@@ -1,181 +1,232 @@
-const tablet = () => {
+const navbarTablet = () => {
   let openedIdx;
-  const menuItems = gsap.utils.toArray('.nav-block')
-  menuItems.pop()
-  const drawerItems = gsap.utils.toArray('.navbar-drawer-content-div.tablet')
+  const menuItems = gsap.utils.toArray(".nav-block");
+  menuItems.pop();
+  const drawerItems = gsap.utils.toArray(".navbar-drawer-content-div.tablet");
 
-  $(window).click(function() {
-    if (openedIdx === undefined) return
-    closeAnimation(menuItems[openedIdx], openedIdx)
+  $(window).click(function () {
+    if (openedIdx === undefined) return;
+    closeAnimation(menuItems[openedIdx], openedIdx);
   });
 
-  $('.navbar-drawer-content-div.tablet').click(function(event) {
+  $(".navbar-drawer-content-div.tablet").click(function (event) {
     event.stopPropagation();
   });
 
-  $('.nav-block').click(function(event) {
+  $(".nav-block").click(function (event) {
     event.stopPropagation();
   });
-
 
   const closeAnimation = (menuItem, idx) => {
-    const timeline = gsap.timeline()
-    timeline.timeScale(5)
-    const [_, symbol] = gsap.utils.toArray(menuItem.children)
+    const timeline = gsap.timeline();
+    timeline.timeScale(5);
+    const [_, symbol] = gsap.utils.toArray(menuItem.children);
 
-    timeline.to(menuItem, {
-      color: "var(--greyscale--fg--subtlest)",
-    }, "<")
+    timeline.to(
+      menuItem,
+      {
+        color: "var(--greyscale--fg--subtlest)",
+      },
+      "<"
+    );
 
-    timeline.to(symbol, { rotation: 0 }, "<")
+    timeline.to(symbol, { rotation: 0 }, "<");
 
-    timeline.to(drawerItems[idx], {
-      height: 0
-    }, "<")
-  }
-
+    timeline.to(
+      drawerItems[idx],
+      {
+        height: 0,
+      },
+      "<"
+    );
+  };
 
   const openAnimation = (menuItem, idx) => {
-    const timeline = gsap.timeline()
-    timeline.timeScale(5)
-    const [_, symbol] = gsap.utils.toArray(menuItem.children)
+    const timeline = gsap.timeline();
+    timeline.timeScale(5);
+    const [_, symbol] = gsap.utils.toArray(menuItem.children);
 
-    timeline.to(menuItem, {
-      color: "var(--brand--fg--neutral)"
-    }, "<")
-    timeline.to(symbol, {
-      rotation: 45
-    }, "<")
+    timeline.to(
+      menuItem,
+      {
+        color: "var(--brand--fg--neutral)",
+      },
+      "<"
+    );
+    timeline.to(
+      symbol,
+      {
+        rotation: 45,
+      },
+      "<"
+    );
     drawerItems.map((drawerItems, drawerIdx) => {
       if (drawerIdx !== idx) {
-        timeline.to(drawerItems, { height: 0 }, "<")
+        timeline.to(drawerItems, { height: 0 }, "<");
       }
-    })
+    });
     menuItems.map((otherMenuItem, omIdx) => {
       if (omIdx !== idx) {
-        timeline.to(otherMenuItem, {
-          color: "var(--greyscale--fg--subtlest)",
-        }, "<")
-        const [_, otherMenuItemSym] = gsap.utils.toArray(otherMenuItem.children)
-        timeline.to(otherMenuItemSym, { rotation: 0 }, "<")
+        timeline.to(
+          otherMenuItem,
+          {
+            color: "var(--greyscale--fg--subtlest)",
+          },
+          "<"
+        );
+        const [_, otherMenuItemSym] = gsap.utils.toArray(
+          otherMenuItem.children
+        );
+        timeline.to(otherMenuItemSym, { rotation: 0 }, "<");
       }
-    })
-    timeline.to(drawerItems[idx], {
-      height: 'auto'
-    }, "<")
-  }
+    });
+    timeline.to(
+      drawerItems[idx],
+      {
+        height: "auto",
+      },
+      "<"
+    );
+  };
 
   menuItems.map((menuItem, idx) => {
     menuItem.onclick = () => {
       if (openedIdx !== idx) {
-        openAnimation(menuItem, idx)
-        openedIdx = idx
+        openAnimation(menuItem, idx);
+        openedIdx = idx;
       } else {
-        closeAnimation(menuItem, idx)
-        openedIdx = undefined
+        closeAnimation(menuItem, idx);
+        openedIdx = undefined;
       }
-    }
-  })
-
-}
+    };
+  });
+};
 
 const desktop = () => {
-  const menuItems = gsap.utils.toArray('.nav-block')
-  const drawerItems = gsap.utils.toArray('.navbar-drawer-content-div.desktop')
+  const menuItems = gsap.utils.toArray(".nav-block");
+  const drawerItems = gsap.utils.toArray(".navbar-drawer-content-div.desktop");
   const heightMap = {
     0: "18rem",
     1: "24rem",
     2: "12rem",
     3: "14rem",
-    4: "18rem"
-  }
+    4: "18rem",
+  };
 
   let openedIdx;
 
-  menuItems.pop()
+  menuItems.pop();
 
-  $(window).click(function() {
-    if (openedIdx === undefined) return
-    closeAnimation(menuItems[openedIdx], openedIdx)
+  $(window).click(function () {
+    if (openedIdx === undefined) return;
+    closeAnimation(menuItems[openedIdx], openedIdx);
   });
 
-  $('.navbar-drawer').click(function(event) {
+  $(".navbar-drawer").click(function (event) {
     event.stopPropagation();
   });
 
-  $('.nav-block').click(function(event) {
+  $(".nav-block").click(function (event) {
     event.stopPropagation();
   });
 
   const closeAnimation = (menuItem, idx) => {
-    const timeline = gsap.timeline()
-    timeline.timeScale(5)
-    const [_, symbol] = gsap.utils.toArray(menuItem.children)
-    timeline.to(menuItem, {
-      color: "var(--brand--fg--neutral)"
-    }, "<")
-    timeline.to('.navbar-drawer', { height: "0" })
-    timeline.to(menuItem, {
-      color: "var(--greyscale--fg--subtlest)",
-    }, "<")
-    timeline.to(symbol, { rotation: 0 }, "<")
-    timeline.to(drawerItems[idx], {
-      height: 0
-    }, "<")
-  }
+    const timeline = gsap.timeline();
+    timeline.timeScale(5);
+    const [_, symbol] = gsap.utils.toArray(menuItem.children);
+    timeline.to(
+      menuItem,
+      {
+        color: "var(--brand--fg--neutral)",
+      },
+      "<"
+    );
+    timeline.to(".navbar-drawer", { height: "0" });
+    timeline.to(
+      menuItem,
+      {
+        color: "var(--greyscale--fg--subtlest)",
+      },
+      "<"
+    );
+    timeline.to(symbol, { rotation: 0 }, "<");
+    timeline.to(
+      drawerItems[idx],
+      {
+        height: 0,
+      },
+      "<"
+    );
+  };
 
   const openAnimation = (menuItem, idx) => {
-    const timeline = gsap.timeline()
-    timeline.timeScale(5)
-    const [_, symbol] = gsap.utils.toArray(menuItem.children)
-    timeline.to(menuItem, {
-      color: "var(--brand--fg--neutral)"
-    }, "<")
-    timeline.to(symbol, { rotation: 45 }, "<")
+    const timeline = gsap.timeline();
+    timeline.timeScale(5);
+    const [_, symbol] = gsap.utils.toArray(menuItem.children);
+    timeline.to(
+      menuItem,
+      {
+        color: "var(--brand--fg--neutral)",
+      },
+      "<"
+    );
+    timeline.to(symbol, { rotation: 45 }, "<");
     menuItems.map((otherMenuItem, omIdx) => {
       if (omIdx !== idx) {
-        timeline.to(otherMenuItem, {
-          color: "var(--greyscale--fg--subtlest)",
-        }, "<")
-        const [_, otherMenuItemSym] = gsap.utils.toArray(otherMenuItem.children)
-        timeline.to(otherMenuItemSym, { rotation: 0 }, "<")
+        timeline.to(
+          otherMenuItem,
+          {
+            color: "var(--greyscale--fg--subtlest)",
+          },
+          "<"
+        );
+        const [_, otherMenuItemSym] = gsap.utils.toArray(
+          otherMenuItem.children
+        );
+        timeline.to(otherMenuItemSym, { rotation: 0 }, "<");
       }
-    })
+    });
 
     drawerItems.map((drawerItem, drawerIdx) => {
       if (drawerIdx !== idx) {
-        timeline.to(drawerItem, {
-          height: 0,
-        }, "<")
+        timeline.to(
+          drawerItem,
+          {
+            height: 0,
+          },
+          "<"
+        );
       }
-    })
-    timeline.to('.navbar-drawer', { height: heightMap[idx] })
-    timeline.to(drawerItems[idx], {
-      height: 'auto'
-    }, "<")
-  }
+    });
+    timeline.to(".navbar-drawer", { height: heightMap[idx] });
+    timeline.to(
+      drawerItems[idx],
+      {
+        height: "auto",
+      },
+      "<"
+    );
+  };
 
   menuItems.map((menuItem, idx) => {
     menuItem.onclick = () => {
       if (openedIdx !== idx) {
-        openAnimation(menuItem, idx)
-        openedIdx = idx
+        openAnimation(menuItem, idx);
+        openedIdx = idx;
       } else {
-        closeAnimation(menuItem, idx)
-        openedIdx = undefined
+        closeAnimation(menuItem, idx);
+        openedIdx = undefined;
       }
-    }
-  })
-}
-
+    };
+  });
+};
 
 const navBar = () => {
   const isTablet = document.body.clientWidth <= 991;
-  if (isTablet) return tablet()
+  if (isTablet) return navbarTablet();
 
-  return desktop()
-}
+  return desktop();
+};
 
-$(document).ready(navBar)
-$(window).on('resize', navBar);
-
+$(document).ready(navBar);
+$(window).on("resize", navBar);
