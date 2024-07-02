d5e51be9c616aacfe3d6813d1834e897ee2908a0
Ian Marquez Aleph Labs <ian.regil@aleph-labs.com>
2024-07-02 17:00:40 +0800

    Update: added pill tabs behavior

--- main/components/hero-banner/index.js
+++ main/components/hero-banner/index.js
@@ -1,16 +1,19 @@
 const heroBanner = () => {
-  var swiper = new Swiper(".hero-banner-swiper", {
-    spaceBetween: 30,
-    loop: true,
-    autoplay: {
-      pauseOnMouseEnter: true,
-      delay: 5000,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  setTimeout(() => {
+
+    var swiper = new Swiper(".hero-banner-swiper", {
+      spaceBetween: 30,
+      loop: true,
+      autoplay: {
+        pauseOnMouseEnter: true,
+        delay: 5000,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }, 200)
 }
 
 
---
+++ main/components/pill-tabs/index.js
@@ -0,0 +1,69 @@
+let openedPillIndex = 0;
+let isMobilePillTabs = document.body.clientWidth <= 767;
+
+function hidePillContent(pill) {
+  const [header, content] = gsap.utils.toArray(pill.children)
+  const iconContainer = header.children[1]
+  const verticalBar = iconContainer.children[1]
+  const bounds = header.getBoundingClientRect()
+
+  const hideTimeline = gsap.timeline()
+  hideTimeline.timeScale(2)
+
+  hideTimeline
+    .to(content,
+      { ...(isMobilePillTabs ? { height: 0 } : { width: 0 }) },
+    )
+    .to(pill, {
+      ...(isMobilePillTabs ? { height: 80 } : { width: bounds.width }),
+      flex: 'none'
+    }, '<')
+    .to(verticalBar, { rotation: 90 }, '<')
+}
+
+function showPillContent(pill) {
+  const [header, content] = gsap.utils.toArray(pill.children)
+  const iconContainer = header.children[1]
+  const verticalBar = iconContainer.children[1]
+  const showTimeline = gsap.timeline({
+    onStart: function() {
+      gsap.set(pill, { clearProps: "flex" });
+    }
+  });
+  showTimeline.timeScale(2)
+
+  showTimeline
+    .to(content, {
+      ...(isMobilePillTabs ? { height: 'auto' } : { width: 'auto' })
+    })
+    .from(content, { opacity: 0 }, "<")
+    .to(pill,
+      {
+        ...(isMobilePillTabs ? { height: 'auto' } : { width: 'auto' })
+      }, "<")
+    .to(verticalBar, { rotation: 0 }, "<")
+}
+
+function PillTabsDesktop() {
+  isMobile = document.body.clientWidth <= 767;
+
+  const pills = gsap.utils.toArray('.pill')
+  const [_, two, three] = pills
+
+  hidePillContent(two)
+  hidePillContent(three)
+
+  pills.map((pill, idx) => {
+    pill.onclick = () => {
+      if (idx !== openedPillIndex) {
+        showPillContent(pill)
+        hidePillContent(pills[openedPillIndex])
+        openedPillIndex = idx;
+      }
+    }
+  })
+
+};
+
+$(window).on("resize", PillTabsDesktop);
+$(document).ready(PillTabsDesktop);
