4c3ec0eecaf190465ce16f819a7b0d7cbcf13aef
Ian Marquez Aleph Labs <ian.regil@aleph-labs.com>
2024-07-05 15:28:10 +0800

    Chore: removed stagger

--- main/components/cta-section-with-bg/index.js
+++ main/components/cta-section-with-bg/index.js
@@ -2,10 +2,10 @@ function CTASectionWithBG() {
   const backgroundBlocks = gsap.utils.toArray('.background-blocks')
   const tlCan = gsap.timeline({ repeat: -1, yoyo: true });
   tlCan
-    .to(backgroundBlocks, 3, { y: '-=5', x: '+=5', ease: Power1.easeInOut, stagger: 0.5 })
-    .to(backgroundBlocks, 2, { y: '+=5', x: '-=5', ease: Power1.easeInOut, stagger: 0.5 })
-    .to(backgroundBlocks, 3, { y: '-=5', ease: Power1.easeInOut, stagger: 0.5 })
-    .to(backgroundBlocks, 3, { y: '+=5', ease: Power1.easeInOut, stagger: 0.5 })
+    .to(backgroundBlocks, 3, { y: '-=5', x: '+=5', ease: Power1.easeInOut })
+    .to(backgroundBlocks, 2, { y: '+=5', x: '-=5', ease: Power1.easeInOut })
+    .to(backgroundBlocks, 3, { y: '-=5', ease: Power1.easeInOut })
+    .to(backgroundBlocks, 3, { y: '+=5', ease: Power1.easeInOut })
 }
 
 
