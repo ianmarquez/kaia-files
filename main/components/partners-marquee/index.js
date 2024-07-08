84881bbae5439bb0bec88e8964a5fdf64c71cfb5
Ian Marquez Aleph Labs <ian.regil@aleph-labs.com>
2024-07-08 13:38:46 +0800

    Update: added partners marquee to start only when nearly visible

--- main/components/partners-marquee/index.js
+++ main/components/partners-marquee/index.js
@@ -1,7 +1,20 @@
-$(window).on("load", () => setTimeout(() => {
-  const firstItems = gsap.utils.toArray('.partner-showcase-item.first')
-  const secondItems = gsap.utils.toArray('.partner-showcase-item.second')
-  horizontalLoop(secondItems, { speed: 0.5 });
-  horizontalLoop(firstItems, { reversed: true, speed: 0.5 });
-}, 500))
+let partnerMarqueeStarted = false;
+function PartnerMarquee() {
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: '.partner-showcase-section',
+      start: "top-=300 center",
+      end: "top-=300 center",
+      onEnter: () => {
+        if (partnerMarqueeStarted) return
+        const firstItems = gsap.utils.toArray('.partner-showcase-item.first')
+        const secondItems = gsap.utils.toArray('.partner-showcase-item.second')
+        horizontalLoop(secondItems, { speed: 0.5 });
+        horizontalLoop(firstItems, { reversed: true, speed: 0.5 });
+        partnerMarqueeStarted = !partnerMarqueeStarted
+      }
+    },
+  });
+}
+$(window).on("load", PartnerMarquee)
 
