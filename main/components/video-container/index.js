e5fd492c1b33ee37d09bca50e87933bc6240c654
Ian Marquez Aleph Labs <ian.regil@aleph-labs.com>
2024-07-31 18:00:45 +0800

    Update: added autoplay when component is visible

---
+++ main/components/video-container/index.js
@@ -0,0 +1,18 @@
+
+function videoContainer() {
+  const iframe = document.getElementById('video-intro')
+  gsap.timeline({
+    onComplete: () => {
+      const currentSrc = iframe.getAttribute('src')
+      iframe.src = currentSrc + '&autoplay=1'
+    },
+    scrollTrigger: {
+      trigger: '.video-container',
+      start: 'top center',
+      end: 'top center',
+    }
+  })
+  console.log(iframe)
+}
+
+$(document).ready(videoContainer)
