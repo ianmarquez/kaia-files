8b84e47015a3a2c9ac45f9b9e1df489c19bb0b62
Ian Marquez Aleph Labs <ian.regil@aleph-labs.com>
2024-07-31 15:15:52 +0800

    Update: moved on chain numbers logic from inline to cdn

--- main/components/onchain-numbers/index.js
+++ main/components/onchain-numbers/index.js
@@ -1,52 +1,74 @@
 function OnChainNumbers() {
-	console.log("dsada");
-	function randomIntFromInterval(min, max) {
-		// min and max included
-		return Math.floor(Math.random() * (max - min + 1) + min);
-	}
+  function randomIntFromInterval(min, max) {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
-	const headings = gsap.utils.toArray(".onchain-heading");
-	const imageContainers = gsap.utils.toArray(".onchain-image-container");
-	const MAPPING = [
-		"market_cap",
-		"transaction_per_sec",
-		"average_block_time",
-		"average_tx_per_block",
-	];
+  const headings = gsap.utils.toArray(".onchain-heading");
+  const imageContainers = gsap.utils.toArray(".onchain-image-container");
+  const MAPPING = [
+    "marketcap",
+    "total_transactions",
+    "active_wallets",
+    "active_contract_count",
+  ];
 
-	$.ajax({
-		url: "https://klaytn.foundation/wp-json/klaytn/v1/main",
-	}).done(function (response) {
-		headings.map((heading, headingIdx) => {
-			let content = response[MAPPING[headingIdx]];
-			if (MAPPING[headingIdx] === "market_cap") {
-				content = (Math.abs(Number(content)) / 1.0e6).toFixed(2) + "M";
-			}
-			const loadingIndicators = gsap.utils.toArray(".loading-spinner");
-			loadingIndicators.forEach((indicator) => indicator.remove());
+  $.ajax({
+    url: "https://api-homepage.kaia.io/analytics",
+  }).done(function(response) {
+    const responseData = response.data;
+    function formatNumber(num, suffix = "") {
+      return Number(num) + suffix;
+    }
 
-			heading.style.display = "block";
-			gsap.to(heading, {
-				scrollTrigger: heading,
-				innerText: content,
-				duration: 5,
-				snap: {
-					innerText: 1,
-				},
-			});
-		});
-	});
+    headings.map((heading, headingIdx) => {
+      let content = responseData[MAPPING[headingIdx]];
+      let suffix = "";
+      if (
+        MAPPING[headingIdx] === "marketcap" ||
+        MAPPING[headingIdx] === "total_transactions" ||
+        MAPPING[headingIdx] === "active_wallets"
+      ) {
+        content = (Math.abs(Number(content)) / 1.0e6).toFixed(2);
+        suffix = "M+";
+      }
+      if (MAPPING[headingIdx] === "active_contract_count") {
+        content = (Number(content) / 1000).toFixed(2);
+        suffix = "K+";
+      }
+      const loadingIndicators = gsap.utils.toArray(".loading-spinner");
+      loadingIndicators.forEach((indicator) => indicator.remove());
 
-	imageContainers.map((imageContainer) => {
-		const imageTimeline = gsap.timeline({});
-		imageTimeline.to(imageContainer.children, {
-			opacity: 1,
-			ease: "power3.out",
-			stagger: randomIntFromInterval(5, 8),
-			repeat: -1,
-			yoyo: true,
-		});
-	});
+      heading.style.display = "block";
+      gsap.to(
+        {
+          value: 0,
+        },
+        {
+          value: content,
+          scrollTrigger: heading,
+          duration: 5,
+          snap: {
+            value: 1,
+          },
+          onUpdate: function() {
+            heading.innerHTML = formatNumber(this.targets()[0].value, suffix);
+          },
+        }
+      );
+    });
+  });
+
+  imageContainers.map((imageContainer) => {
+    const imageTimeline = gsap.timeline({});
+    imageTimeline.to(imageContainer.children, {
+      opacity: 1,
+      ease: "power3.out",
+      // stagger: randomIntFromInterval(5, 8),
+      repeat: -1,
+      yoyo: true,
+    });
+  });
 }
 
 $(document).ready(OnChainNumbers);
